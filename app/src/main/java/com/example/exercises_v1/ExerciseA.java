7b447d4980a6e366cfe1a1a66e04c20477137b61
Leati Perise <Perisi02@myunitec.ac.nz>
2026-05-28 18:18:57 +1200

    Foundational layout for exercise a complete. Started work on the 'Create New' button, getting the textfield to appear/disappeaer etc

--- app/src/main/java/com/example/exercises_v1/ExerciseA.java
+++ app/src/main/java/com/example/exercises_v1/ExerciseA.java
@@ -1,6 +1,12 @@
 package com.example.exercises_v1;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ExerciseA extends AppCompatActivity {
+    LinearLayout textFields;
+
+    Button btn_createNew;
+    Button btn_cancelAdd;
+    Button btn_okayAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,17 @@ public class ExerciseA extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textFields = findViewById(R.id.ll_textFields);
+        btn_createNew = findViewById(R.id.btn_createNew);
+        btn_cancelAdd = findViewById(R.id.btn_cancelAdd);
+        btn_okayAdd = findViewById(R.id.btn_confirmAdd);
+    }
+
+    public void openAddOperation(View view) {
+        textFields.setVisibility(VISIBLE);
+        btn_okayAdd.setVisibility(VISIBLE);
+        btn_cancelAdd.setVisibility(VISIBLE);
+        btn_createNew.setVisibility(GONE);
     }
 }
